810c09609eece0ec7c46b647ce292301a9a238d7
FenFM <HohesCAlt@gmail.com>
2025-02-11 20:21:24 +0100

    Stuff

--- Exercise-06/VLSI-Files/vhdl/cpu.vhd
+++ Exercise-06/VLSI-Files/vhdl/cpu.vhd
@@ -17,7 +17,6 @@ end entity cpu;
 
 architecture structure of cpu is
     -- signal for the program counter
---    signal s_pc_dout     : std_logic_vector( C_BIT_WIDTH-1 downto 0 );
     signal s_branch_zero : std_logic;
     signal s_sign_shift  : std_logic_vector( C_BIT_WIDTH-1 downto 0 );
 
@@ -28,7 +27,7 @@ architecture structure of cpu is
     signal s_alu_zero_flag     : std_logic;
     signal s_alu_overflow_flag : std_logic;
 
-    -- signal for the instruction memory
+    -- signals for the instruction memory
     signal s_instruction_memory_write_data : std_logic_vector( C_BIT_WIDTH-1 downto 0 ) ;
     signal s_instruction_memory_write_addr : std_logic_vector( log2(C_IM_MEM_SIZE)-1 downto 0 );
     signal s_instruction_memory_write_rden : std_logic;    
@@ -36,8 +35,13 @@ architecture structure of cpu is
     signal s_instruction_memory_read_addr  : std_logic_vector( log2(C_IM_MEM_SIZE)-1 downto 0 );
     signal s_instruction_memory_read_rden  : std_logic;  
 
-    -- signal for the data memory
-    signal s_data_memory_read_data  : std_logic_vector( C_BIT_WIDTH-1 downto 0 );
+    -- signals for the data memory    
+    signal s_data_memory_write_data : std_logic_vector( C_BIT_WIDTH-1 downto 0 ) ;
+    signal s_data_memory_write_addr : std_logic_vector( log2(C_DM_MEM_SIZE)-1 downto 0 );
+    signal s_data_memory_write_rden : std_logic;    
+    signal s_data_memory_read_data  : std_logic_vector( C_BIT_WIDTH-1 downto 0 ) ;
+    signal s_data_memory_read_addr  : std_logic_vector( log2(C_DM_MEM_SIZE)-1 downto 0 );
+    signal s_data_memory_read_rden  : std_logic; 
 
     -- signals for the register file
     signal s_register_file_read_a_data : std_logic_vector( C_REG_SIZE-1 downto 0 );
@@ -45,25 +49,27 @@ architecture structure of cpu is
     signal s_register_write_data       : std_logic_vector( C_REG_SIZE-1 downto 0 );
 
     -- signals for the control unit
-    signal s_cu_pc_src   : std_logic;
-    signal s_cu_branch   : std_logic;
-    signal s_cu_alu_op   : std_logic_vector( C_BIT_WIDTH-1 downto 0 );
-    signal s_cu_alu_src  : std_logic;
-    signal s_cu_reg_wren : std_logic;
-    signal s_cu_mem_wren : std_logic;
-    signal s_cu_mem_rden : std_logic;
+    signal s_cu_pc_src     : std_logic;
+    signal s_cu_branch     : std_logic;
+    signal s_cu_alu_op     : std_logic_vector( C_BIT_WIDTH-1 downto 0 );
+    signal s_cu_alu_src    : std_logic;
+    signal s_cu_reg_wren   : std_logic;
+    signal s_cu_mem_wren   : std_logic;
+    signal s_cu_mem_rden   : std_logic;
+    signal s_cu_mem_to_reg : std_logic;
 
 
 begin
     CU : entity work.control_unit
     port map(
-        o_pc_src    =>  s_cu_pc_src,
-        o_branch    =>  s_cu_branch,
-        o_alu_op    =>  s_cu_alu_op,
-        o_alu_src   =>  s_cu_alu_src,
-        o_reg_wren  =>  s_cu_reg_wren,
-        o_mem_wren  =>  s_cu_mem_wren,
-        o_mem_rden  =>  s_cu_mem_rden
+        o_pc_src      =>  s_cu_pc_src,
+        o_branch      =>  s_cu_branch,
+        o_alu_op      =>  s_cu_alu_op,
+        o_alu_src     =>  s_cu_alu_src,
+        o_reg_wren    =>  s_cu_reg_wren,
+        o_mem_wren    =>  s_cu_mem_wren,
+        o_mem_rden    =>  s_cu_mem_rden,
+        o_mem_to_reg  =>  s_cu_mem_to_reg
     );
 
     PC : entity work.program_counter
@@ -117,7 +123,7 @@ begin
         o_zero_flag      =>  s_alu_zero_flag,
         o_overflow_flag  =>  s_alu_overflow_flag
     );
-    s_alu_operand_a <= s_register_file_read_a_data
+    s_alu_operand_a <= s_register_file_read_a_data;
 
 
     ALU_MUX : entity work.mux_switch_2
@@ -127,7 +133,7 @@ begin
         a  =>  s_register_file_read_b_data,
         b  =>  s_sign_shift,
         o  =>  s_alu_operand_b
-    )
+    );
 
 
     DM : entity work.memory
@@ -139,10 +145,10 @@ begin
         clk           =>  clk,
         i_write_data  =>  s_register_file_read_b_data,        
         i_write_addr  =>  s_alu_result, 
-        i_write_wren  =>  ,
+        i_write_wren  =>  s_cu_mem_wren,
         o_read_data   =>  s_data_memory_read_data,       
-        i_read_addr   =>  ,
-        i_read_rden   =>                                       
+        i_read_addr   =>  s_alu_result,
+        i_read_rden   =>  s_cu_mem_rden
     );
 
 
@@ -155,7 +161,7 @@ begin
         clk            =>  clk,
         i_write_data   =>  s_register_write_data,
         i_write_addr   =>  s_instruction_memory_read_data,
-        i_write_wren   =>  ,
+        i_write_wren   =>  s_cu_reg_wren,
         o_read_a_data  =>  s_register_file_read_a_data,
         i_read_a_addr  =>  s_instruction_memory_read_data,
         o_read_b_data  =>  s_register_file_read_b_data,
@@ -166,8 +172,8 @@ begin
     MEM_to_REG_MUX : entity work.mux_switch_2
     generic map( bit_width  =>  C_BIT_WIDTH )
     port map(
-        s  =>  ,
-        a  =>  s_pc_dout,
+        s  =>  s_cu_mem_to_reg,
+        a  =>  s_alu_result,
         b  =>  s_data_memory_read_data,
         o  =>  s_register_write_data
     );
